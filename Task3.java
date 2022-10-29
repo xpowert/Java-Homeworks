26269a5624abaa39c6ea99ff8cce7732861f2f25
Eogame <xpowertlatvia@gmail.com>
2022-10-29 20:30:05 +0300

    Pievienota code mape
    Pievienota code mape, lai veiksmīgi izpildītu 3. md.

---
+++ Task3.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+class Task3{
+
+    public static void main(String args[]) {
+        String name = "Marcis";
+        int varda_garums = name.length();
+
+        System.out.println(varda_garums);
+        //File myFile = new File(null, name)
+    }
+}
