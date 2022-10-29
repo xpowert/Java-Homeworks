86dd178d72d8c646e364586389f74aa87bdb6d35
Eogame <xpowertlatvia@gmail.com>
2022-10-29 20:58:55 +0300

    Papildinājumi 3.md.
    Izveidota klase
    Inicializēts mainīgie un File objekts

--- Task3.java
+++ Task3.java
@@ -1,12 +1,23 @@
 import java.io.File;
 
-class Task3{
+class Task3 {
 
     public static void main(String args[]) {
+
+        // Inicializējam String un int mainīgos
         String name = "Marcis";
+
+        // Ar length metodi iegūstam String garumu
         int varda_garums = name.length();
 
+        // Izvada vārda garumu
         System.out.println(varda_garums);
-        //File myFile = new File(null, name)
+
+        // Inicializējam jaunu File objektu un norādām attiecīgo failu, ņemot vērā vārda garumu
+        File myFile = new File("..\\codes\\file" + varda_garums + ".txt");
+
+        // Izmantojot getName() metodi izprintējam faila nosaukumu. 
+        System.out.println(myFile.getName());
+
     }
 }
