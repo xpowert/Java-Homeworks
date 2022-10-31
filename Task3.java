9f9127eb036ff07cffdfe843ea1fe8e218302f0a
xpowert <xpowertlatvia@gmail.com>
2022-10-31 18:27:29 +0200

    3.md pirmais uzdevums
    Pabeigts 3.md pirmais uzdevums

--- Task3.java
+++ Task3.java
@@ -1,23 +1,82 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 class Task3 {
 
+    String name;
+
+    // setName metode kas piešķir vērtību parametram name
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    // Metode kas atgriež vārda garumu
+    int nameLenght() {
+        return this.name.length();
+    }
+
+    // myFile metode kas atgriež File objektu, ņemot vērā burtu skaitu definētajā
+    // vārda un ceļu uz failu
+    public File myFile() {
+        // tiek norādīts ceļš uz failu ņemot vērā name burtu skaitu.
+        File myFile = new File(".\\codes\\file" + this.nameLenght() + ".txt");
+        return myFile;
+    }
+
+    public int countLines() {
+        int lines = 0;
+        try {
+            Scanner sc = new Scanner(this.myFile());
+            // Iterē cauri faila rindām
+            while (sc.hasNextLine()) {
+                lines++;
+                sc.nextLine();
+            }
+            // Aizver failu
+            sc.close();
+            // Atgriež rindu skaitu
+            return lines;
+            // Kļūdas gadījuma, parāda kļūdu
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return lines;
+        }
+    }
+
+    // Atgriež faila nosaukumu bez paplašinājuma
+    public String fileName(String name) {
+        // Izveidojam jaunu masīvu kurā glabāsies faila nosaukums un paplašinājums
+        // Ar split atdala string vadoties pēc punkta stringā
+        String[] fileName = name.split("\\.");
+
+        // Atgriež pirmo elementu no masīva
+        return fileName[0];
+    }
+
     public static void main(String args[]) {
 
-        // Inicializējam String un int mainīgos
-        String name = "Marcis";
+        // Inicializējam objektu Task3
+        Task3 foo = new Task3();
 
-        // Ar length metodi iegūstam String garumu
-        int varda_garums = name.length();
+        // Uzstādām vārdu
+        foo.setName("Marcis");
 
-        // Izvada vārda garumu
-        System.out.println(varda_garums);
+        // Izprintējam vārdu un vārda garumu
+        System.out.println("Vards " + foo.getName() + " Varda garums - " + foo.nameLenght());
 
-        // Inicializējam jaunu File objektu un norādām attiecīgo failu, ņemot vērā vārda garumu
-        File myFile = new File("..\\codes\\file" + varda_garums + ".txt");
+        // Izprintē atrasto failu
+        System.out.println(foo.myFile().getName());
 
-        // Izmantojot getName() metodi izprintējam faila nosaukumu. 
-        System.out.println(myFile.getName());
+        // Izprintē faila nosaukumu bez paplašinājuma
+        System.out.println(foo.fileName(foo.myFile().getName()));
+
+        // Izprintē rindu skaitu failā
+        System.out.println(foo.countLines());
 
     }
 }
