5dc39c425af5d17cf4f2eebbaf3aea24e27094b9
xpowert <xpowertlatvia@gmail.com>
2022-11-07 09:51:09 +0200

    Pabeigts 4.md.
    Izpildīti visi 4.md. nosacījumi.

--- Task4.java
+++ Task4.java
@@ -8,10 +8,8 @@ public class Task4 {
 
     // Inicializējam skaitļu sarakstu
     ArrayList<Integer> revenues = new ArrayList<Integer>();
-    // Inicializējam teksta sarakstu
-    ArrayList<String> usedServices = new ArrayList<String>();
-    // Pārvērš teksta sarakstu unikāla teksta sarakstā
-    Set<String> uniqServices = new HashSet<String>(usedServices);
+    // Inicializējam unikāla teksta sarakstā
+    Set<String> uniqServices = new HashSet<String>();
 
     // Metodes
 
@@ -26,14 +24,72 @@ public class Task4 {
         this.revenues.add(revenue);
     }
 
-    void addUsedService(String service){
-        
+    // Metode kas pievieno izmantoto servisu sarakstam
+    void addUsedService(String service) {
+        // Izveidojam masīvu ar servisiem
+        String[] services = { "hosting", "photoshop", "web administration", "spotify", "mailchimp" };
+
+        // Pārbauda vai ievadītais serviss atbilst sarakstā izmantotajiem servisiem
+        // Ievadīto servisu automātiski pārvēršam uz lowerCase, lai ievadot atbilstošu
+        // servisu ar lielo burtu, tas tiktu atrasts sarakstā
+        if (Arrays.asList(services).contains(service.toLowerCase())) {
+            // Ievietojam sarakstā servisu ar mazajiem burtiem, lai pēc tam būtu vieglāk
+            // atrast nepieciešamo servisu
+            this.uniqServices.add(service.toLowerCase());
+        } else
+            System.out.println("Sads " + service + " serviss netiek izmantots!");
     }
+
+    // Metode kas atgriež Revenues saraksta summu
+    int sumRevenue() {
+        // Pārbaudam vai saraksts nav tukšs
+        if (this.revenues.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        // Iterējam cauri sarakstam un saskaitām kopā visus elementus
+        for (int i = 0; i < this.revenues.size(); i++) {
+            sum += this.revenues.get(i);
+        }
+        // Tiek atgriezta kopējo skaitļu summa
+        return sum;
+    }
+
+    // Metode kas atgriež true, ja ievadītais vārds ir sarakstā
+    boolean checkService(String servicName) {
+        // pārbayda vai sarakstā ir norādītais string, kas ir konvertēs uz mazajiem
+        // burtiem.
+        if (this.uniqServices.contains(servicName.toLowerCase())) {
+            return true;
+        } else
+            return false;
+    }
+
     public static void main(String args[]) {
         // Izveidojam jaunu objektu
         Task4 system = new Task4();
+        Task4 system2 = new Task4();
+        Task4 system3 = new Task4();
 
         system.addRevenue(-55);
+        system.addRevenue(400);
+        system.addRevenue(100);
+        system.addRevenue(256);
+
+        system.addUsedService("Hosting");
+        system.addUsedService("Spotify");
+
+        system2.addRevenue(500);
+        system2.addRevenue(341);
+
+        system3.addUsedService("GIT Pro");
+        system3.addUsedService("Mailchimp");
+        system3.addUsedService("Spotify");
+        System.out.println(system3.checkService("GIT Pro"));
+
+        System.out.println(system.sumRevenue());
+        System.out.println(system2.sumRevenue());
+        System.out.println(system.checkService("SpotiFy"));
 
     }
 }
